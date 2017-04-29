52292ab50888e96a3153355aba64c58f85697c39
lhw1995 <916347995@qq.com>
2017-04-29 11:59:17 +0800

    测试

---
+++ src/main/java/com/douban/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.douban.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by lhw2 on 2017/4/29.
+ */
+@Controller
+public class TestController {
+    @RequestMapping("/test")
+    public String test(){
+        return "index";
+    }
+}
+
